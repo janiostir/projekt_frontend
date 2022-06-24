4e0bdfee2987f537ced18965eb8ad486e0fe77bb
janiostir <jani.ostir@gmail.com>
2022-06-24 12:10:28 +0200

    prvi commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Nav />
+
+      <main>
+
+        <Welcome />
+
+        <div className="album py-5 bg-light">
+          <div className="container">
+
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+              <Card />
+
+            </div>
+          </div>
+        </div>
+
+      </main>
+
+      <Footer />
+
+    </>
   );
 }
 
