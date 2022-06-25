88a28169b799881b1733f15a1816752246a8a5c4
janiostir <jani.ostir@gmail.com>
2022-06-25 15:03:49 +0200

    replys

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 
-const Card = ({cardData}:{cardData:any}) => {
+interface CardProps{
+    cardData: any
+    hideControls?: boolean;
+}
+
+const Card = ({cardData, hideControls}: CardProps) => {
     return(
         <>
             <div className="col">
                 <div className="card shadow-sm">
-                    <img src={require('../img.jpg')} alt="Thumbnail" />
+                    {!hideControls && <img src={require('../img.jpg')} alt="Thumbnail" />}
                     <div className="card-body">
                         <h5>{cardData.title}</h5>
                         <p className="card-text">{cardData.content}</p>
                         <div className="d-flex justify-content-between align-items-center">
                             <div className="btn-group">
-                                <a href={"/post/"+cardData.id}><button type="button" className="btn btn-sm btn-outline-secondary">View</button></a>
+                                {!hideControls && <a href={"/post/"+cardData.id}><button type="button" className="btn btn-sm btn-outline-secondary">View</button></a>}
                             </div>
                         </div>
                     </div>
--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,42 +1,62 @@
-import React, {SyntheticEvent, useEffect, useState} from "react";
-import {UserDto} from "../classes/user.dto";
-import {PostDto} from "../classes/post.dto";
-import {useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Params, useParams} from "react-router-dom";
 import axios from "axios";
 import Card from "../components/Card";
-
 
 const Post = () => {
 
     const[cardData,setCards] = useState([]);
-
+    const[replies,setReplies] = useState([]);
     const { id } = useParams();
 
-    const loadCards = async () => {
-        const res = await axios.get('http://localhost:8080/post/'+id,{withCredentials: true});
-        console.log(res);
-        if (res.status === 200) {
-            console.log(res.data);
+    const loadPosts = async () => {
+        const res = await axios.get('http://localhost:8080/post/'+id, {withCredentials: true});
+        if (res.status == 200){
             setCards(res.data);
         }
     }
 
-    useEffect(()=>{
-        loadCards();
-    },[]);
+    useEffect(() => {
+        loadPosts();
+    }, []);
+
+    const loadReplies = async () => {
+        const res = await axios.get('http://localhost:8080/post/replies/' + id, {withCredentials: true});
+        if (res.status == 200){
+            setReplies(res.data);
+        }
+    }
+
+    useEffect(() => {
+        loadReplies();
+    }, []);
+
+    if(cardData && replies.length > 0) {
+        return (
+            <>
+                <Card cardData={cardData} hideControls={true}/>
+
+                {replies.map((reply: any, i) => {
+                    return <Card cardData={reply} key={i} hideControls={true}/>
+                })
+                }
+            </>
+        );
+    }
 
     if(cardData) {
         return (
             <>
-                <Card cardData={cardData} />
+                <Card cardData={cardData} hideControls={true}/>
             </>
         );
     }
 
     return (
         <>
-            <h1>Ni objav</h1>
+
         </>
     );
 }
+
 export default Post;
